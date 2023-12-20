6c4817476b3f3c233cf9b1edbd179b4e235c8a72
=safyan999 <=safyantahir780@gmail.com>
2023-12-20 18:53:34 +0500

    restaturant website

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <signUP/>
+  </div>
+</template>
+
+<script>
+import signUP from './components/signUP.vue';
+
+export default {
+  name: 'App',
+  components: {
+    signUP
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/signUP.vue
@@ -0,0 +1,67 @@
+<template>
+    
+    <div class="register">
+        <h1>Sign UP</h1>
+        <form>
+
+            <input type="text" placeholder="safyan" name="name"  v-model="name" >
+            <input type="text" placeholder="user@email.com" name="gmail" v-model="email">
+            <input type="text" placeholder="password" name="password" v-model="password">
+            <button  v-on:click="registerBtn" type="button">
+                SignUp
+            </button>
+        </form>
+    </div>
+    
+</template>
+<script>
+export default{
+    name:"signUp",
+
+
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+
+    },
+    method:{
+        registerBtn(){
+           console.log("submit");
+        }
+    }
+}
+</script>
+<style>
+.register{
+    align-items: center;
+    width: 50%;
+    text-align: center;
+    border: 1px solid black;
+    margin-left: 20%;
+    margin-top: 50px;
+    
+}
+.register input{
+    display: block;
+    margin:20px;
+    padding: 20px;
+    width:80%;
+  
+
+
+
+
+}
+.register button{
+    width: 40%;
+    padding:5px;
+    margin-bottom: 10px;
+    background-color: aquamarine;
+    font-size: larger;
+    border-radius: 20px;
+}
+
+</style>
